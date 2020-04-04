b957c1770e53514dab85f7c2dc67bc5330f319d1
Eemeli Surakka <37372229+Eemelinen@users.noreply.github.com>
2020-04-04 20:48:57 +0200

    CRUD

---
+++ CRUD/login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<?php
+    if(isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        if($username && $password) {
+            echo $username;
+            echo "\n";
+            echo $password;
+        } else {
+            echo "No data";
+        }
+        $connection = mysqli_connect('localhost', 'root', '', 'loginapp');    
+    
+        if($connection) {
+            echo 'connected';
+        } else {
+            echo "Not connected";
+        }
+    }
+?>
+
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        
+        <div class="container">
+            <div  class="col-sm-6">
+                <form action="login.php" method="post">
+                    <div class="form-group"> 
+                        <label for="username">Username</label>
+                        <input name="username" type="text" placeholder="username" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input name="password" type="password" placeholder="password" class="form-control" />
+                    </div>
+                    <input type="submit" name="submit" class="btn btn-success" value="Submit" />
+                </form>
+            </div>
+        </div>
+
+    </body>
+</html>
+
